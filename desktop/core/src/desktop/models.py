803dc2f536fa186bb10709979eef643a9966b4a2
Prakash Ranade <ranade@cloudera.com>
2017-12-13 15:13:40 -0800

    To upgrade Django 1.8, following changes are needed:
    - Django models now accept function datetime.now instead of datetime.today()
    - ForeignKey with unique=True needs to redefined as OneToOneField
    - django.db.backends has been renamed to django.db.backends.base.base
    - LaxOptionParser is no more available, use CommandParser instead
    - generic moved from django.contrib.contenttypes.generic to django.contrib.contenttypes.fields
    - django.forms.util moved to django.forms.utils
    - Fixing dtz.now instance as method call
    - Adding Django Mako backend in settings.py
    - Adding django hack for getting context processors
    - Removing manually added Django-1.7/django/middleware/security.py file
    - Enabling pylint test to work
    - Adding pylint argument processing change

--- desktop/core/src/desktop/models.py
+++ desktop/core/src/desktop/models.py
@@ -30,7 +30,7 @@ except ImportError:
 from itertools import chain
 
 from django.contrib.auth import models as auth_models
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse, NoReverseMatch
@@ -610,7 +610,7 @@ class Document(models.Model):
 
   content_type = models.ForeignKey(ContentType)
   object_id = models.PositiveIntegerField()
-  content_object = generic.GenericForeignKey('content_type', 'object_id')
+  content_object = GenericForeignKey('content_type', 'object_id')
 
   objects = DocumentManager()
 
@@ -1076,7 +1076,7 @@ class Document2(models.Model):
 
   parent_directory = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
 
-  doc = generic.GenericRelation(Document, related_query_name='doc_doc') # Compatibility with Hue 3
+  doc = GenericRelation(Document, related_query_name='doc_doc') # Compatibility with Hue 3
 
   objects = Document2Manager()
 
