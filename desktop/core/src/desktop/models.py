42c00dc75801cae7ca3c73ff2556f7f3259ff322
Prakash Ranade <ranade@cloudera.com>
2017-12-13 15:13:40 -0800

    Fixing Django1.8 upgrade requirements:
      - Django models now accept function datetime.now instead of datetime.today()
      - ForeignKey with unique=True needs to redefined as OneToOneField
      - django.db.backends has been renamed to django.db.backends.base.base
      - LaxOptionParser is no more available
      - from django.contrib.contenttypes import generic moved to from django.contrib.contenttypes.fields import GenericRelation
      - request.META.get('HTTP_X_REQUESTED_WITH') need to be replaced with request.is_ajax()
      - django.forms.util moved to django.forms.utils
      - django.template.Context moved to django.template.context.Context
      - django.core.context_processors moved to django.template.context_processors
      - django.utils.tzinfo moved to django.utils.timezone
      - TEMPLATES variable more details : https://docs.djangoproject.com/en/1.8/topics/templates/
      - adding djangomako.backends.MakoBackend
      - adding standard_context_processors into RequestContext
      - Adding Django 1.8's django.middleware.security.SecurityMiddleware middleware

--- desktop/core/src/desktop/models.py
+++ desktop/core/src/desktop/models.py
@@ -30,7 +30,7 @@ except ImportError:
 from itertools import chain
 
 from django.contrib.auth import models as auth_models
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import (GenericRelation, GenericForeignKey)
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse, NoReverseMatch
@@ -610,7 +610,7 @@ class Document(models.Model):
 
   content_type = models.ForeignKey(ContentType)
   object_id = models.PositiveIntegerField()
-  content_object = generic.GenericForeignKey('content_type', 'object_id')
+  content_object = GenericForeignKey('content_type', 'object_id')
 
   objects = DocumentManager()
 
@@ -1077,7 +1077,7 @@ class Document2(models.Model):
 
   parent_directory = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
 
-  doc = generic.GenericRelation(Document, related_query_name='doc_doc') # Compatibility with Hue 3
+  doc = GenericRelation(Document, related_query_name='doc_doc') # Compatibility with Hue 3
 
   objects = Document2Manager()
 
@@ -1915,7 +1915,7 @@ class Cluster():
 def _get_apps(user, section=None):
   current_app = None
   other_apps = []
-  if user.is_authenticated():
+  if user and user.is_authenticated():
     apps = appmanager.get_apps(user)
     apps_list = appmanager.get_apps_dict(user)
     for app in apps:
