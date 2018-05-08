shubhampachori12110095/hue

To upgrade Django 1.10, following changes are needed:
- django.conf.urls.patterns are deprecated.
- fixing django.core.urlresolvers
- Adding name argument in "urls.py"
- static file processing.
- Fixing Django Template context processor.
- Fixing name property in reverse url access
- Renaming password_policy to hue_password_policy
- Renaming get_password_validators to hue_get_password_validators
- Adding debug messages for app based urls.py
- Adding reverse url fix
- upgrading devtools to latest version
- Adding astroid[1.5.3] python module to devtools
- fixing enum issue for latest Enum python package
- fixing django management commands, test_runners and test_windmill

desktop/libs/metadata/src/metadata/urls.py
@@ -15,51 +15,53 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.conf.urls import patterns, url
-
+from django.conf.urls import url
+from metadata import navigator_api as metadata_navigator_api
+from metadata import optimizer_api as metadata_optimizer_api
+from metadata import workload_analytics_api as metadata_workload_analytics_api
 
 # Navigator API
-urlpatterns = patterns('metadata.navigator_api',
-  url(r'^api/navigator/search_entities/?$', 'search_entities', name='search_entities'),
-  url(r'^api/navigator/search_entities_interactive/?$', 'search_entities_interactive', name='search_entities_interactive'),
-  url(r'^api/navigator/find_entity/?$', 'find_entity', name='find_entity'),
-  url(r'^api/navigator/get_entity/?$', 'get_entity', name='get_entity'),
-  url(r'^api/navigator/add_tags/?$', 'add_tags', name='add_tags'),
-  url(r'^api/navigator/delete_tags/?$', 'delete_tags', name='delete_tags'),
-  url(r'^api/navigator/list_tags/?$', 'list_tags', name='list_tags'),
-  url(r'^api/navigator/suggest/?$', 'suggest', name='suggest'),
-  url(r'^api/navigator/update_properties/?$', 'update_properties', name='update_properties'),
-  url(r'^api/navigator/delete_properties/?$', 'delete_properties', name='delete_properties'),
-  url(r'^api/navigator/lineage/?$', 'get_lineage', name='get_lineage'),
-)
+urlpatterns = [
+  url(r'^api/navigator/search_entities/?$', metadata_navigator_api.search_entities, name='search_entities'),
+  url(r'^api/navigator/search_entities_interactive/?$', metadata_navigator_api.search_entities_interactive, name='search_entities_interactive'),
+  url(r'^api/navigator/find_entity/?$', metadata_navigator_api.find_entity, name='find_entity'),
+  url(r'^api/navigator/get_entity/?$', metadata_navigator_api.get_entity, name='get_entity'),
+  url(r'^api/navigator/add_tags/?$', metadata_navigator_api.add_tags, name='add_tags'),
+  url(r'^api/navigator/delete_tags/?$', metadata_navigator_api.delete_tags, name='delete_tags'),
+  url(r'^api/navigator/list_tags/?$', metadata_navigator_api.list_tags, name='list_tags'),
+  url(r'^api/navigator/suggest/?$', metadata_navigator_api.suggest, name='suggest'),
+  url(r'^api/navigator/update_properties/?$', metadata_navigator_api.update_properties, name='update_properties'),
+  url(r'^api/navigator/delete_properties/?$', metadata_navigator_api.delete_properties, name='delete_properties'),
+  url(r'^api/navigator/lineage/?$', metadata_navigator_api.get_lineage, name='get_lineage'),
+]
 
 
 # Optimizer API
-urlpatterns += patterns('metadata.optimizer_api',
-  url(r'^api/optimizer/upload/history/?$', 'upload_history', name='upload_history'),
-  url(r'^api/optimizer/upload/query/?$', 'upload_query', name='upload_query'),
-  url(r'^api/optimizer/upload/table_stats/?$', 'upload_table_stats', name='upload_table_stats'),
-  url(r'^api/optimizer/upload/status/?$', 'upload_status', name='upload_status'),
+urlpatterns += [
+  url(r'^api/optimizer/upload/history/?$', metadata_optimizer_api.upload_history, name='upload_history'),
+  url(r'^api/optimizer/upload/query/?$', metadata_optimizer_api.upload_query, name='upload_query'),
+  url(r'^api/optimizer/upload/table_stats/?$', metadata_optimizer_api.upload_table_stats, name='upload_table_stats'),
+  url(r'^api/optimizer/upload/status/?$', metadata_optimizer_api.upload_status, name='upload_status'),
 
   #v2
-  url(r'^api/optimizer/get_tenant/?$', 'get_tenant', name='get_tenant'),
+  url(r'^api/optimizer/get_tenant/?$', metadata_optimizer_api.get_tenant, name='get_tenant'),
 
-  url(r'^api/optimizer/top_databases/?$', 'top_databases', name='top_databases'),
-  url(r'^api/optimizer/top_tables/?$', 'top_tables', name='top_tables'),
-  url(r'^api/optimizer/top_columns/?$', 'top_columns', name='top_columns'),
-  url(r'^api/optimizer/top_joins/?$', 'top_joins', name='top_joins'),
-  url(r'^api/optimizer/top_filters/?$', 'top_filters', name='top_filters'),
-  url(r'^api/optimizer/top_aggs/?$', 'top_aggs', name='top_aggs'),
+  url(r'^api/optimizer/top_databases/?$', metadata_optimizer_api.top_databases, name='top_databases'),
+  url(r'^api/optimizer/top_tables/?$', metadata_optimizer_api.top_tables, name='top_tables'),
+  url(r'^api/optimizer/top_columns/?$', metadata_optimizer_api.top_columns, name='top_columns'),
+  url(r'^api/optimizer/top_joins/?$', metadata_optimizer_api.top_joins, name='top_joins'),
+  url(r'^api/optimizer/top_filters/?$', metadata_optimizer_api.top_filters, name='top_filters'),
+  url(r'^api/optimizer/top_aggs/?$', metadata_optimizer_api.top_aggs, name='top_aggs'),
 
-  url(r'^api/optimizer/table_details/?$', 'table_details', name='table_details'),
+  url(r'^api/optimizer/table_details/?$', metadata_optimizer_api.table_details, name='table_details'),
 
-  url(r'^api/optimizer/query_risk/?$', 'query_risk', name='query_risk'),
-  url(r'^api/optimizer/query_compatibility/?$', 'query_compatibility', name='query_compatibility'),
-  url(r'^api/optimizer/similar_queries/?$', 'similar_queries', name='similar_queries'),
-)
+  url(r'^api/optimizer/query_risk/?$', metadata_optimizer_api.query_risk, name='query_risk'),
+  url(r'^api/optimizer/query_compatibility/?$', metadata_optimizer_api.query_compatibility, name='query_compatibility'),
+  url(r'^api/optimizer/similar_queries/?$', metadata_optimizer_api.similar_queries, name='similar_queries'),
+]
 
 
 # Workload Analytics API
-urlpatterns += patterns('metadata.workload_analytics_api',
-  url(r'^api/workload_analytics/get_operation_execution_details/?$', 'get_operation_execution_details', name='get_operation_execution_details'),
-)
+urlpatterns += [
+  url(r'^api/workload_analytics/get_operation_execution_details/?$', metadata_workload_analytics_api.get_operation_execution_details, name='get_operation_execution_details'),
+]

huebuild/hue_mirror

desktop/libs/metadata/src/metadata/urls.py
@@ -15,56 +15,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.conf.urls import patterns, url
-
+from django.conf.urls import url
+from metadata import navigator_api as metadata_navigator_api
+from metadata import optimizer_api as metadata_optimizer_api
+from metadata import workload_analytics_api as metadata_workload_analytics_api
 
 # Navigator API
-urlpatterns = patterns('metadata.navigator_api',
-  url(r'^api/navigator/search_entities/?$', 'search_entities', name='search_entities'),
-  url(r'^api/navigator/search_entities_interactive/?$', 'search_entities_interactive', name='search_entities_interactive'),
-  url(r'^api/navigator/find_entity/?$', 'find_entity', name='find_entity'),
-  url(r'^api/navigator/get_entity/?$', 'get_entity', name='get_entity'),
-  url(r'^api/navigator/add_tags/?$', 'add_tags', name='add_tags'),
-  url(r'^api/navigator/delete_tags/?$', 'delete_tags', name='delete_tags'),
-  url(r'^api/navigator/list_tags/?$', 'list_tags', name='list_tags'),
-  url(r'^api/navigator/suggest/?$', 'suggest', name='suggest'),
-  url(r'^api/navigator/update_properties/?$', 'update_properties', name='update_properties'),
-  url(r'^api/navigator/delete_metadata_properties/?$', 'delete_metadata_properties', name='delete_metadata_properties'),
-  url(r'^api/navigator/lineage/?$', 'get_lineage', name='get_lineage'),
-  url(r'^api/navigator/namespace/create/?$', 'create_namespace', name='create_namespace'),
-  url(r'^api/navigator/namespace/?$', 'get_namespace', name='get_namespace'),
-  url(r'^api/navigator/namespace/property/create/?$', 'create_namespace_property', name='create_namespace_property'),
-  url(r'^api/navigator/namespace/property/map/?$', 'map_namespace_property', name='map_namespace_property'),
-  url(r'^api/navigator/models/properties/mappings/?$', 'get_model_properties_mapping', name='get_model_properties_mapping'),
-)
-
+urlpatterns = [
+  url(r'^api/navigator/search_entities/?$', metadata_navigator_api.search_entities, name='search_entities'),
+  url(r'^api/navigator/search_entities_interactive/?$', metadata_navigator_api.search_entities_interactive, name='search_entities_interactive'),
+  url(r'^api/navigator/find_entity/?$', metadata_navigator_api.find_entity, name='find_entity'),
+  url(r'^api/navigator/get_entity/?$', metadata_navigator_api.get_entity, name='get_entity'),
+  url(r'^api/navigator/add_tags/?$', metadata_navigator_api.add_tags, name='add_tags'),
+  url(r'^api/navigator/delete_tags/?$', metadata_navigator_api.delete_tags, name='delete_tags'),
+  url(r'^api/navigator/list_tags/?$', metadata_navigator_api.list_tags, name='list_tags'),
+  url(r'^api/navigator/suggest/?$', metadata_navigator_api.suggest, name='suggest'),
+  url(r'^api/navigator/update_properties/?$', metadata_navigator_api.update_properties, name='update_properties'),
+  url(r'^api/navigator/delete_metadata_properties/?$', metadata_navigator_api.delete_metadata_properties, name='delete_metadata_properties'),
+  url(r'^api/navigator/lineage/?$', metadata_navigator_api.get_lineage, name='get_lineage'),
+]
 
 # Optimizer API
-urlpatterns += patterns('metadata.optimizer_api',
-  url(r'^api/optimizer/upload/history/?$', 'upload_history', name='upload_history'),
-  url(r'^api/optimizer/upload/query/?$', 'upload_query', name='upload_query'),
-  url(r'^api/optimizer/upload/table_stats/?$', 'upload_table_stats', name='upload_table_stats'),
-  url(r'^api/optimizer/upload/status/?$', 'upload_status', name='upload_status'),
+urlpatterns += [
+  url(r'^api/optimizer/upload/history/?$', metadata_optimizer_api.upload_history, name='upload_history'),
+  url(r'^api/optimizer/upload/query/?$', metadata_optimizer_api.upload_query, name='upload_query'),
+  url(r'^api/optimizer/upload/table_stats/?$', metadata_optimizer_api.upload_table_stats, name='upload_table_stats'),
+  url(r'^api/optimizer/upload/status/?$', metadata_optimizer_api.upload_status, name='upload_status'),
 
   #v2
-  url(r'^api/optimizer/get_tenant/?$', 'get_tenant', name='get_tenant'),
+  url(r'^api/optimizer/get_tenant/?$', metadata_optimizer_api.get_tenant, name='get_tenant'),
 
-  url(r'^api/optimizer/top_databases/?$', 'top_databases', name='top_databases'),
-  url(r'^api/optimizer/top_tables/?$', 'top_tables', name='top_tables'),
-  url(r'^api/optimizer/top_columns/?$', 'top_columns', name='top_columns'),
-  url(r'^api/optimizer/top_joins/?$', 'top_joins', name='top_joins'),
-  url(r'^api/optimizer/top_filters/?$', 'top_filters', name='top_filters'),
-  url(r'^api/optimizer/top_aggs/?$', 'top_aggs', name='top_aggs'),
+  url(r'^api/optimizer/top_databases/?$', metadata_optimizer_api.top_databases, name='top_databases'),
+  url(r'^api/optimizer/top_tables/?$', metadata_optimizer_api.top_tables, name='top_tables'),
+  url(r'^api/optimizer/top_columns/?$', metadata_optimizer_api.top_columns, name='top_columns'),
+  url(r'^api/optimizer/top_joins/?$', metadata_optimizer_api.top_joins, name='top_joins'),
+  url(r'^api/optimizer/top_filters/?$', metadata_optimizer_api.top_filters, name='top_filters'),
+  url(r'^api/optimizer/top_aggs/?$', metadata_optimizer_api.top_aggs, name='top_aggs'),
 
-  url(r'^api/optimizer/table_details/?$', 'table_details', name='table_details'),
+  url(r'^api/optimizer/table_details/?$', metadata_optimizer_api.table_details, name='table_details'),
 
-  url(r'^api/optimizer/query_risk/?$', 'query_risk', name='query_risk'),
-  url(r'^api/optimizer/query_compatibility/?$', 'query_compatibility', name='query_compatibility'),
-  url(r'^api/optimizer/similar_queries/?$', 'similar_queries', name='similar_queries'),
-)
+  url(r'^api/optimizer/query_risk/?$', metadata_optimizer_api.query_risk, name='query_risk'),
+  url(r'^api/optimizer/query_compatibility/?$', metadata_optimizer_api.query_compatibility, name='query_compatibility'),
+  url(r'^api/optimizer/similar_queries/?$', metadata_optimizer_api.similar_queries, name='similar_queries'),
+]
 
 
 # Workload Analytics API
-urlpatterns += patterns('metadata.workload_analytics_api',
-  url(r'^api/workload_analytics/get_operation_execution_details/?$', 'get_operation_execution_details', name='get_operation_execution_details'),
-)
+urlpatterns += [
+  url(r'^api/workload_analytics/get_operation_execution_details/?$', metadata_workload_analytics_api.get_operation_execution_details, name='get_operation_execution_details'),
+]