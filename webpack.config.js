a7e9d70eef57be789e84d02c6c15a84dc5fc0ee4
Johan Ahlen <johan@johanahlen.com>
2019-08-20 14:23:00 +0200

    HUE-8940 [frontend] Add production mode to webpack build
    It's now possible to generate optimized webpack bundles for production by adding -- --env.production to the build commands, for example:
    
    npm run webpack -- --env.production

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const BundleTracker = require('webpack-bundle-tracker');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CleanObsoleteChunks = require('webpack-clean-obsolete-chunks');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const merge = require('webpack-merge');
 
 const path = require('path');
 const each = require('lodash/fp/each');
@@ -25,8 +26,7 @@ class RelativeBundleTracker extends BundleTracker {
   }
 }
 
-module.exports = {
-  devtool: 'source-map',
+const SHARED = {
   mode: 'development',
   performance: {
     maxEntrypointSize: 400 * 1024, // 400kb
@@ -38,26 +38,6 @@ module.exports = {
     alias: {
       bootstrap: __dirname + '/node_modules/bootstrap-2.3.2/js'
     }
-  },
-  entry: {
-    hue: ['./desktop/core/src/desktop/js/hue.js'],
-    notebook: ['./desktop/core/src/desktop/js/apps/notebook/app.js'],
-    tableBrowser: ['./desktop/core/src/desktop/js/apps/table_browser/app.js']
-  },
-  optimization: {
-    //minimize: true,
-    minimize: false,
-    splitChunks: {
-      chunks: 'all'
-    },
-    runtimeChunk: {
-      name: 'hue'
-    }
-  },
-  output: {
-    path: __dirname + '/desktop/core/src/desktop/static/desktop/js/bundles/hue',
-    filename: '[name]-bundle-[hash].js',
-    chunkFilename: '[name]-chunk-[hash].js'
   },
   module: {
     rules: [
@@ -72,14 +52,94 @@ module.exports = {
       }
     ]
   },
-
+  output: {
+    filename: '[name]-bundle-[hash].js'
+  },
+  optimization: {
+    minimize: false
+  },
   plugins: [
     // new BundleAnalyzerPlugin({ analyzerPort: 9000 }),
     new CleanObsoleteChunks(),
-    new CleanWebpackPlugin([__dirname + '/desktop/core/src/desktop/static/desktop/js/bundles/hue']),
-    new RelativeBundleTracker({ path: '.', filename: 'webpack-stats.json' }),
     new webpack.BannerPlugin(
       '\nLicensed to Cloudera, Inc. under one\nor more contributor license agreements.  See the NOTICE file\ndistributed with this work for additional information\nregarding copyright ownership.  Cloudera, Inc. licenses this file\nto you under the Apache License, Version 2.0 (the\n"License"); you may not use this file except in compliance\nwith the License.  You may obtain a copy of the License at\n\nhttp://www.apache.org/licenses/LICENSE-2.0\n\nUnless required by applicable law or agreed to in writing, software\ndistributed under the License is distributed on an "AS IS" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.\n'
     )
   ]
+};
+
+const PRODUCTION = {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimize: true
+  },
+  performance: {
+    hints: false
+  }
+};
+
+module.exports = (env, args) => {
+  let baseConfig = env && env.production ? merge(SHARED, PRODUCTION) : SHARED;
+  return [
+    merge(baseConfig, {
+      name: 'hue',
+      entry: {
+        hue: ['./desktop/core/src/desktop/js/hue.js'],
+        notebook: ['./desktop/core/src/desktop/js/apps/notebook/app.js'],
+        tableBrowser: ['./desktop/core/src/desktop/js/apps/table_browser/app.js']
+      },
+      optimization: {
+        splitChunks: {
+          chunks: 'all'
+        },
+        runtimeChunk: {
+          name: 'hue'
+        }
+      },
+      output: {
+        path: __dirname + '/desktop/core/src/desktop/static/desktop/js/bundles/hue',
+        chunkFilename: '[name]-chunk-[hash].js'
+      },
+      plugins: [
+        new CleanWebpackPlugin([
+          __dirname + '/desktop/core/src/desktop/static/desktop/js/bundles/hue'
+        ]),
+        new RelativeBundleTracker({ path: '.', filename: 'webpack-stats.json' })
+      ]
+    }),
+    merge(baseConfig, {
+      name: 'login',
+      entry: {
+        login: ['./desktop/core/src/desktop/js/login.js']
+      },
+      output: {
+        path: __dirname + '/desktop/core/src/desktop/static/desktop/js/bundles/login'
+      },
+      plugins: [
+        new CleanWebpackPlugin([
+          __dirname + '/desktop/core/src/desktop/static/desktop/js/bundles/login'
+        ]),
+        new RelativeBundleTracker({ path: '.', filename: 'webpack-stats-login.json' })
+      ]
+    }),
+    merge(baseConfig, {
+      name: 'workers',
+      target: 'webworker',
+      entry: {
+        sqlLocationWebWorker: ['./desktop/core/src/desktop/js/sql/sqlLocationWebWorker.js'],
+        sqlSyntaxWebWorker: ['./desktop/core/src/desktop/js/sql/sqlSyntaxWebWorker.js']
+      },
+      output: {
+        path: __dirname + '/desktop/core/src/desktop/static/desktop/js/bundles/workers',
+        chunkFilename: '[name]-chunk-[hash].js',
+        globalObject: 'this'
+      },
+      plugins: [
+        new CleanWebpackPlugin([
+          __dirname + '/desktop/core/src/desktop/static/desktop/js/bundles/workers'
+        ]),
+        new RelativeBundleTracker({ path: '.', filename: 'webpack-stats-workers.json' })
+      ]
+    })
+  ];
 };
