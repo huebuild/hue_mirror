42651af71dbd1765d0bf642634e9fb93e8592247
Romain <romain.rigaux@gmail.com>
2019-09-18 08:36:54 -0700

    HUE-8737 [py3] Workaround bytes set as default values in migration charfields

--- desktop/core/src/desktop/migrations/0002_initial.py
+++ desktop/core/src/desktop/migrations/0002_initial.py
@@ -22,9 +22,9 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('app', models.CharField(db_index=True, help_text='App that this configuration belongs to.', max_length=32)),
-                ('properties', models.TextField(default=b'[]', help_text='JSON-formatted default properties values.')),
+                ('properties', models.TextField(default='[]', help_text='JSON-formatted default properties values.')),
                 ('is_default', models.BooleanField(db_index=True, default=False)),
-                ('groups', models.ManyToManyField(db_index=True, db_table=b'defaultconfiguration_groups', to='auth.Group')),
+                ('groups', models.ManyToManyField(db_index=True, db_table='defaultconfiguration_groups', to='auth.Group')),
             ],
             options={
                 'ordering': ['app', '-is_default', 'user'],
